f950d9cb31bffa22996c58b54e0ad4fd9fa8fe56
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2023-10-12 00:37:04 +0300

    Dosya eklemek için action eklendi

--- Persistance/E-CommerceAPI.API/Controllers/ProductsController.cs
+++ Persistance/E-CommerceAPI.API/Controllers/ProductsController.cs
@@ -1,14 +1,10 @@
-﻿using E_CommerceAPI.Application.Abstraction;
-using E_CommerceAPI.Application.Repositories;
+﻿using E_CommerceAPI.Application.Repositories;
+using E_CommerceAPI.Application.RequestParameters;
 using E_CommerceAPI.Application.ViewModels.Products;
 using E_CommerceAPI.Domain.Entities;
-using E_CommerceAPI.Persistance.Concreate;
 using E_CommerceAPI.Persistance.Repositories;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
-using System.Net.Http;
-using System.Runtime.InteropServices;
 
 namespace E_CommerceAPI.API.Controllers
 {
@@ -18,27 +14,43 @@ namespace E_CommerceAPI.API.Controllers
     {
         private readonly IProductReadRepository _productReadService;
         private readonly IProductWriteRepository _productWriteRepository;
+        IWebHostEnvironment _webHostEnvironment;
 
 
-        public ProductsController(IProductReadRepository productService, IProductWriteRepository productWriteRepository)
+        public ProductsController(IProductReadRepository productService, IProductWriteRepository productWriteRepository, IWebHostEnvironment webHostEnvironment)
         {
             this._productReadService = productService;
             _productWriteRepository = productWriteRepository;
+            _webHostEnvironment = webHostEnvironment;
 
         }
         //[Guid("0B99B2A9-7DEB-48DC-BC15-01B5180FC0F5")]
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll([FromQuery]Pagination pagination)
         {
-            var products = _productReadService.GetAll(false);
-            return Ok(products);
+            var totalCount = _productReadService.GetAll(false).Count();
+            var products = _productReadService.GetAll(true).Skip(pagination.Size * pagination.Page).Take(pagination.Size).Select(p => new
+            {
+               p.Id,
+                p.Name,
+                p.Description,
+                p.Stock,
+                p.Price               
+
+
+            }).ToList();
+            return Ok(new
+            {
+                totalCount,
+                products
+            });
         }
-        [HttpGet("{id}")]
-        public async Task<IActionResult> Get(string id)
-        {
-            var product =await _productReadService.GetByIdAsync(id,false);
-            return Ok(product);
-        }
+        //[HttpGet("{id}")]
+        //public async Task<IActionResult> Get(string id)
+        //{
+        //    var product = await _productReadService.GetByIdAsync(id, false);
+        //    return Ok(product);
+        //}
         [HttpPost]
         public async Task<IActionResult> AddProduct(VM_Product_Create model)
         {
@@ -48,6 +60,7 @@ namespace E_CommerceAPI.API.Controllers
             }
             await _productWriteRepository.AddAsync(new()
             {
+
                 Name = model.Name,
                 Description = model.Description,
                 Stock = model.Stock,
@@ -75,6 +88,23 @@ namespace E_CommerceAPI.API.Controllers
         {
             var x = await _productWriteRepository.DeleteAsync(id);         
             await _productWriteRepository.SaveChanges();            
+            return Ok();
+        }
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Upload()
+        {
+            string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "resource/product-images");
+            if(!Directory.Exists(uploadPath))
+                Directory.CreateDirectory(uploadPath);
+            Random r = new();
+            foreach (IFormFile file in Request.Form.Files)
+            {
+                
+                string fullPath = Path.Combine(uploadPath, $"{r.Next()}{Path.GetExtension(file.FileName)}");
+                using FileStream fileStream = new(fullPath, FileMode.Create, FileAccess.Write, FileShare.None, 1024*1024,useAsync:false); 
+                await file.CopyToAsync(fileStream);
+                await fileStream.FlushAsync();
+            }
             return Ok();
         }
       
